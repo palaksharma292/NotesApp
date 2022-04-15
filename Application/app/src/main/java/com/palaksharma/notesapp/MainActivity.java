2e6b432fc8cd9ebc8b118717a7f261e81aad2173
faizan <faizanshafiq123@gmail.com>
2022-04-15 13:44:21 -0600

    Action Bar Custom

--- Application/app/src/main/java/com/palaksharma/notesapp/MainActivity.java
+++ Application/app/src/main/java/com/palaksharma/notesapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.palaksharma.notesapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        leftIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Toast.makeText(MainActivity.this,"You clicked left icon", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void AddNote(View view)
