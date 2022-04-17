a3e876f341b9bd722726e9361a383300745c0fbd
faizan <faizanshafiq123@gmail.com>
2022-04-17 15:49:14 -0600

    no message

---
+++ Application/app/src/main/java/com/palaksharma/notesapp/NotesViewActivity.java
@@ -0,0 +1,14 @@
+package com.palaksharma.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_view);
+    }
+}
